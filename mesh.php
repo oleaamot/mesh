7f6ae3ef9843dd7f6c126e5e30ba009ba33a8e8d
Ole Aamot <oka+www@oka.no>
2013-11-25 20:13:35 +0100

    Clean cruft.

--- mesh.php
+++ mesh.php
@@ -1,14 +1,13 @@
 <pre>
 <?
-/* $Id$                                                                                                                                               
-                                                                                                                                                      
-   dugnadsnett.no - Find nearest geographical nodes for 'N' nodes                                                                                     
-                                                                                                                                                      
-   TODO:                                                                                                                                              
-                                                                                                                                                      
-   - Use real distance calculations, not just absolute difference in linear 2D space.                                                                 
-     http://www.linuxjournal.com/magazine/work-shell-calculating-distance-between-two-latitudelongitude-points                                        
-                                                                                                                                                      
+/*
+  dugnadsnett.no - Find nearest geographical nodes for 'N' nodes
+
+  TODO:
+
+  - Use real distance calculations, not just absolute difference in linear 2D space.
+  http://www.linuxjournal.com/magazine/work-shell-calculating-distance-between-two-latitudelongitude-points
+
 */
 
 class Mesh {
@@ -23,9 +22,9 @@ class Mesh {
 $file = file_get_contents("https://raw.github.com/petterreinholdtsen/meshfx-node/master/oslo-nodes.csv");
 $list = array();
 
-$data = str_getcsv($file, "\n"); //parse the rows                                                                                                     
+$data = str_getcsv($file, "\n"); //parse the rows
 foreach($data as &$row) {
-  $row = str_getcsv($row, "\t"); //parse the items in rows                                                                                            
+  $row = str_getcsv($row, "\t"); //parse the items in rows
   if ($row[0]=="longitude") {
     continue;
   } else {
@@ -40,8 +39,6 @@ foreach($data as &$row) {
   }
 }
 
-// print_r($list);                                                                                                                                    
-
 if (isset($_GET['lat']) && isset($_GET['lon'])) {
   $location->latitude = $_GET['lat'];
   $location->longitude = $_GET['lon'];
@@ -51,8 +48,6 @@ if (isset($_GET['lat']) && isset($_GET['lon'])) {
 }
 
 foreach($list as $node) {
-  // print $node->k;                                                                                                                                  
-  // print $node->comment . "\t" . $node->last_seen . "\t" . $node->latitude . "\t" . $node->longitude . "\t" . $node->mac . "<br />\n";              
   $x[$node->k] = abs((float)$location->latitude-(float)$node->latitude);
   $y[$node->k] = abs((float)$location->longitude-(float)$node->longitude);
   $z[$node->k] = abs((float)($x[$node->k])+(float)($y[$node->k]));
@@ -77,10 +72,7 @@ foreach($keys as $node => $key) {
   foreach($list as $host) {
     if ($host->k == $m->k) {
       $m = $host;
-      // print "<tt>";                                                                                                                                
-      // print $m->k . "\t";                                                                                                                          
       print $m->comment . "\t" . $m->last_seen . "\t" . $m->latitude . "\t" . $m->longitude . "\t" . $m->mac . "\n";
-      // print_r($m);                                                                                                                                 
     }
   }
 }
